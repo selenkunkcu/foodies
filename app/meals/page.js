74a1488167c6905223f156c9fe057bb27d2cdb36
selenkunkcu <selenkunkcu@gmail.com>
2025-09-15 16:52:52 +0300

    Foodies Project

---
+++ app/meals/page.js
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { Suspense } from "react"; //handle loading states and show a fallback content until some data or resource has been loading
+
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+import { getMeals } from "@/lib/meals";
+
+export const metadata = {
+    title: "All Meals",
+    description: "Browse the delicious meals shared by our vibrant community.",
+};
+
+async function Meals() {
+    const meals = await getMeals();
+
+    return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>Choose your favorite receipe and cook it yourself. It is easy and fun!</p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">Share Your Favorite Receipe</Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <Suspense fallback={<p className={classes.loading}>Fetching Meals..</p>}>
+                    <Meals />
+                </Suspense>
+            </main>
+        </>
+    );
+}
---
+++ components/main-header/main-header.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import MainHeaderBackground from "./main-header.background";
+import NavLink from "./nav-link";
+import logoImg from "@/assets/images/logo.png";
+import classes from "./main-header.module.css";
+
+const MainHeader = () => {
+    return (
+        <>
+            <MainHeaderBackground />
+            <header className={classes.header}>
+                <Link className={classes.logo} href="/">
+                    <Image src={logoImg} alt="A plate with food on it" priority />
+                    NextLevel Food
+                </Link>
+
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <NavLink href="/meals">Browse Meals</NavLink>
+                        </li>
+                        <li>
+                            <NavLink href="/community">Foodies Community</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+};
+
+export default MainHeader;
